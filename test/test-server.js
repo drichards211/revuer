b2a8c55bb45fbe39ebff65a1b11cda3b1138b7e2
Daniel Richards <drichards211@yahoo.com>
2019-02-20 23:34:27 -0500

    Fix "app.address is not a function" error
    Tests were failing. test-server.js needed to be updated to reflect
    new runServer() and closeServer() functions.

--- test/test-server.js
+++ test/test-server.js
@@ -1,9 +1,10 @@
 "use strict";
 
+global.DATABASE_URL = 'mongodb://localhost/revuer-db-test'
 const chai = require('chai')
 const expect = chai.expect
 const chaiHttp = require("chai-http")
-const app = require("../server.js")
+const {app, runServer, closeServer} = require('../server');
 
 chai.use(chaiHttp)
 
