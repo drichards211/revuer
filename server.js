42826b1502d181d5c62a1c53c8ab70a163b5d3ec
Daniel Richards <drichards211@yahoo.com>
2019-02-24 18:43:26 -0500

    Fix tests not working
    User authentication tests were failing. Substituted package.json
    values from a Thinkful example and re-ran npm install. Also re-wrote
    runServer() to allow a passed argument for the database name.

--- server.js
+++ server.js
@@ -44,7 +44,25 @@ app.use('*', (req, res) => {
 
 let server
 
-function runServer() {
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err)
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`)
+        resolve()
+      })
+        .on('error', err => {
+          mongoose.disconnect()
+          reject(err)
+        })
+    })
+  })
+}
+
+/* function runServer() {
   return new Promise((resolve, reject) => {
     mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
       if (err) {
@@ -62,7 +80,7 @@ function runServer() {
     })
   })
 }
-
+ */
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,25 @@
 "use strict";
 
-global.DATABASE_URL = 'mongodb://localhost/revuer-db-test'
+require('dotenv').config() // This is the missing line which is needed for .env values to be read.
+// Must be included in all test files.
+/* const {TEST_DATABASE_URL} = 'mongodb://localhost/revuer-db-test' */
+const {JWT_SECRET, TEST_DATABASE_URL} = require('../config')
+/* global.DATABASE_URL = 'mongodb://localhost/revuer-db-test' */
 const chai = require('chai')
 const expect = chai.expect
 const chaiHttp = require("chai-http")
-const {app, runServer, closeServer} = require('../server');
+const {app, runServer, closeServer} = require('../server')
 
 chai.use(chaiHttp)
+
+describe('server starting', function(){
+  before(function() {
+      runServer(TEST_DATABASE_URL)
+  })
+  after(function() {
+  return closeServer()
+  })
+})
 
 describe("index.html page", function() {
   it("should exist", function() {
