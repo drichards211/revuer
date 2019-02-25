8ef1779bc6552fbfc7927b2374dd995a30988f9b
Daniel Richards <drichards211@yahoo.com>
2019-02-24 21:15:22 -0500

    Allow tests to run in TravisCI
    Paths to databases need to be specified in local .env settings.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 'use strict'
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/revuer-db'
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL  || 'mongodb://localhost/revuer-db-test'
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+/* exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/revuer-db' */
+/* exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL  || 'mongodb://localhost/revuer-db-test' */
 /* exports.LOCAL_TEST_DATABASE_URL = 'mongodb://localhost/revuer-db-test' */
 exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET
