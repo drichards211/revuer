fd3e12bc997dd236467c796529d083ec5a94ecf1
Daniel Richards <drichards211@yahoo.com>
2019-03-14 00:18:17 -0400

    Add fetch request for OMDB

--- public/script/add-movie.js
+++ public/script/add-movie.js
@@ -13,46 +13,79 @@ function addMovie() {
   $('.movie-find-form').submit(function(event) {
     console.log('movie-find-form submitted')
     event.preventDefault()
-    const movieTitle = $('#movietitle').val()
-    // fetch request to API
-    searchOMDB()
+    const searchTitle = $('#movietitle').val()
+    // remove leading and trailing whitespace, convert multiple spaces to single, replace all remaining spaces with '+':
+    const formatTitle = searchTitle.trim().replace(/\s\s+/g, ' ').split(' ').join('+');
+    searchOMDB(formatTitle)
   })
 }
 
-function searchOMDB() {
-  // Search OMDB API by Title, returns multiple results:
+function searchOMDB(title) {
+// Search OMDB API by Title, returns multiple results:
   console.log("searchOMDB() ran")
-  apiResults()
-}
-
+  let results
+  const apiKey = localStorage.getItem('omdbApiKey')
+  const fetchUrl = `http://www.omdbapi.com/?apikey=${apiKey}&type=movie&s=${title}`
+  console.log(fetchUrl)
+  
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&type=movie&s=${title}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+          throw new Error(response.statusText)
+      }
+    })
+    .then(responseJson => {
+      if (responseJson.Response === "True") {
+      console.log("Movie(s) found")
+      console.log(responseJson)
+      results = responseJson.Search
+      renderFirstApiResult(results)
+      } else {
+          console.log("No movies found")
+          suggestNewSearch()
+      }
+    })
+    .catch(err => {
+      console.log(`OMDB API failed to fetch: ${err}`)
+      suggestNewSearch()
+    })
+  }
+    
 function lookupOMDB() {
   // Search OMDB API by "IMDB ID", returns one result:
   console.log("retrieveOMDB() ran")
 }
 
-function apiResults() {
-  console.log("renderAPIResults ran")
-  const movieData = "temporary"
+function renderFirstApiResult(results) {
+  console.log("renderFirstApiResult() ran")
+  /* const firstResult = results[0] */
+  const { Poster, Title, Year, imdbID } = results[0]
   $('.video-screen').html(
     `<div class="movie-API-box-1">
       <h2>Is this your movie?</h2>
+      <p>${Title} -- ${Year}</p>
+      <img src="${Poster}" alt="image of ${Title} poster">
+      <p>
       <button class="yes" id="movie-correct">Yes</button>
       <button class="no" id="movie-incorrect">No</button>
+      </p>
     </div>`)
   $('body').on('click', 'button', function(event) {
     if (`${$(this).prop('id')}` === 'movie-correct') {
       console.log("\"Yes\" button pressed")
-      addMovieDetails(movieData)  
+      addMovieDetails(results[0])  
     }
     if (`${$(this).prop('id')}` === 'movie-incorrect') {
       console.log("\"No\" button pressed")
-      moreApiResults()
+      renderMoreApiResults()
     }
   })
 }
 
-function moreApiResults() {
-  console.log("moreApiResults() ran")
+function renderMoreApiResults() {
+  console.log("renderMoreApiResults() ran")
   $('.video-screen').html(
     `<div class="movie-API-box-1">
       <h2>Sorry about that.</h2>
@@ -87,6 +120,7 @@ function moreApiResults() {
     
 function addMovieDetails(movieData) {
   console.log("addMovieDetails() ran")
+  console.log(movieData)
   // update DOM with details form
   // jQuery listen for submit
   // fetch call (possibly a different function)
