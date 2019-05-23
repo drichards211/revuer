8b8fd28e75be1187fd54a2ee0ab8fdf86b93e59f
Daniel Richards <drichards211@yahoo.com>
2019-05-23 14:48:49 -0400

    Add margin-bottom to col-12 and keep dynamic buttons below video screen

--- public/script/index.js
+++ public/script/index.js
@@ -254,6 +254,7 @@ function manageWindowResize() {
   $('.video-screen').css({'height': `calc(${initialMainContainerWidth} * .5625)`})
   $('.chair-button-wrapper').css({'transform': `translateX(calc((${initialChairWidth} * 15 - 100vw)* -.5))`})
   $('.film-button-wrapper').css({'width': `calc(${initialFilmButtonWidth} * 18)`,'transform': `translateX(calc(((${initialFilmButtonWidth} * 18) - 100vw)* -.5))`})
+  $('.keep-below-screen').css({'top': `calc(${initialMainContainerWidth} * .5625 + 3rem)`})
 // update values if window resized:
   window.onresize = function() {
     console.log("Window size changed... resizing theater")
@@ -263,6 +264,7 @@ function manageWindowResize() {
     $('.video-screen').css({'height': `calc(${mainContainerWidth} * .5625)`})
     $('.chair-button-wrapper').css({'transform': `translateX(calc((${chairWidth} * 15 - 100vw)* -.5))`})
     $('.film-button-wrapper').css({'width': `calc(${filmButtonWidth} * 18)`, 'transform': `translateX(calc(((${filmButtonWidth} * 18) - 100vw)* -.5))`})
+    $('.keep-below-screen').css({'top': `calc(${mainContainerWidth} * .5625 + 3rem)`})
   }
 }
 
