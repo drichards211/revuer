d4f30e791ab54d583e390503e2e9cd58c7ed4fa7
Daniel Richards <drichards211@yahoo.com>
2019-09-11 08:18:32 -0400

    Update fittext.js

--- public/script/jquery.fittext.js
+++ public/script/jquery.fittext.js
@@ -7,12 +7,16 @@
 * http://sam.zoy.org/wtfpl/
 *
 * Date: Thu May 05 14:23:00 2011 -0600
+*
+* Modifed by Daniel Richards, 2019
+* "- DR" indicates any changes made
 */
 
 (function( $ ){
 
   $.fn.fitText = function( kompressor, compLevel, options ) {
-
+    // Added compLevel to fine-tune font-sizing on a case-by-case basis - DR
+    
     // Setup options
     var compressor = kompressor || 1,
         settings = $.extend({
@@ -27,10 +31,11 @@
 
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
+        // Setting transition animation to 0s helps some functions to obtain correct size-values - DR
+        $this.css({"transition": "font-size 0s"})
         $this.css('font-size', Math.max(Math.min($this.width() / (eval(compCalc)), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
       };
-      //compressor*13.5
-
+      
       // Call once to set.
       resizer();
 
